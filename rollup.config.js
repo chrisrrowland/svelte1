9d8ad21da8361839fcb9411ad9c02d0e54559e99
Chris Rowland <chris.robert.rowland@gmail.com>
2021-01-26 15:01:45 -0500

    Initial repo

---
+++ rollup.config.js
@@ -0,0 +1,65 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+import typescriptCompiler from 'typescript';
+import sveltePreprocessor from 'svelte-preprocess';
+
+const development = process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    svelte({
+	compilerOptions: {
+		dev: development
+	},
+      extensions: ['.svelte'],
+      preprocess: sveltePreprocessor(),
+      emitCss: true
+    }),
+    postcss({
+      extract: true,
+      minimize: true,
+      use: [
+        ['sass', {
+          includePaths: [
+            './theme',
+            './node_modules'
+          ]
+        }]
+      ]
+    }),
+    typescript({
+      typescript: typescriptCompiler,
+      clean: true
+    }),
+    resolve({
+      browser: true,
+      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+    }),
+    commonjs({ include: 'node_modules/**' }),
+
+    development && serve({
+      contentBase: './public',
+      open: false
+    }),
+
+    development && livereload({ watch: './public' }),
+
+    !development && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
